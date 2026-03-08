349efbe40a8047b11bd3cb9cbc674ad96e615716
Apfelholz <apfelholzz1@gmail.com>
2026-03-08 21:22:15 +0100

    feat: add start screen scene and script for game initialization

---
+++ lost-trails/start_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@onready var start_button = $CenterContainer/Menu/StartButton
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/home_den.tscn")
